1434ef353c7f2ef55a950bd75efc5fcb008446b1
dragonrider23 <keitellf@gmail.com>
2014-12-20 08:07:31 -0600

    Multiple pools can now be created and used together

--- mempool.c
+++ mempool.c
@@ -6,58 +6,86 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <valgrind/memcheck.h>
-#include "ncore.h"
 
+#include "ncore.h"
+#include "mempool.h"
+
+#define MAX_NUM_OF_POOLS 10
 #define POOL_SIZE 12000
 
-typedef struct mem_control_block {
-	bool is_used;
-} mem_control_block;
+static memory_pool* nval_mem_pool[MAX_NUM_OF_POOLS];
 
 static bool ready;
-static int chunks_allocated;
-static void* memory_pool_start;
-static void* memory_pool_end;
-static size_t mem_chunk_size = sizeof(nval) + sizeof(mem_control_block);
+static size_t nval_chunk_size = sizeof(nval) + sizeof(mem_control_block);
 
+/* Allocate first pool */
 void allocate_pool(void) {
-	memory_pool_start = malloc(POOL_SIZE * mem_chunk_size);
-	memory_pool_end = memory_pool_start + (POOL_SIZE * mem_chunk_size) - mem_chunk_size; // Last usable address
-	VALGRIND_CREATE_MEMPOOL(memory_pool_start, 0, 0);
-	chunks_allocated = 0;
+	create_pool(0);
 	ready = true;
 	return;
 }
 
+/* Create new pools at index pnum */
+void create_pool(int pnum) {
+	nval_mem_pool[pnum] = malloc(sizeof(memory_pool));
+	nval_mem_pool[pnum]->mem_chunk_size = nval_chunk_size;
+	nval_mem_pool[pnum]->memory_pool_start = malloc(POOL_SIZE * nval_mem_pool[pnum]->mem_chunk_size);
+	nval_mem_pool[pnum]->memory_pool_last_assignable = nval_mem_pool[pnum]->memory_pool_start + (POOL_SIZE * nval_mem_pool[pnum]->mem_chunk_size) - nval_mem_pool[pnum]->mem_chunk_size; // Last usable address
+	nval_mem_pool[pnum]->memory_pool_end = nval_mem_pool[pnum]->memory_pool_start + (POOL_SIZE * nval_mem_pool[pnum]->mem_chunk_size);
+	nval_mem_pool[pnum]->chunks_allocated = 0;
+	VALGRIND_CREATE_MEMPOOL(nval_mem_pool[pnum]->memory_pool_start, 0, 0);
+	return;
+}
+
+/* Find and return a pointer to an nval sized chunk */
 void* nmalloc(void) {
 	/* If the pool hasn't been allocated, allocate it */
 	if (!ready) {
 		allocate_pool();
 	}
 
-	/* Prepare variables */
-	void* current_location = memory_pool_start;
+	void* current_location;
 	mem_control_block* current_location_mcb;
-	void* memory_location = memory_pool_start;
+	void* memory_location;
 
-	while (current_location < memory_pool_end) {
-		current_location_mcb = current_location;
-		if (current_location_mcb->is_used) {
-			current_location = current_location + mem_chunk_size;
-			continue;
-		} else {
-			current_location_mcb->is_used = true;
-			memory_location = current_location;
+	/* Loop through pools to find a free address */
+	for (int i = 0; i < MAX_NUM_OF_POOLS; i++) {
+		/* If a pool is uninitialized, all pool after it will be too */
+		if (nval_mem_pool[i] == NULL) {
+			break;
+		}
+
+		current_location = nval_mem_pool[0]->memory_pool_start;
+		memory_location = nval_mem_pool[0]->memory_pool_start;
+
+		/* Loop over pool address range to find a free memory chunk */
+		while (current_location < nval_mem_pool[0]->memory_pool_end) {
+			current_location_mcb = current_location;
+			if (current_location_mcb->is_used) {
+				/* Chunk in use */
+				current_location = current_location + nval_mem_pool[0]->mem_chunk_size;
+				continue;
+			} else {
+				/* Chunk is not in use or is unitialized */
+				current_location_mcb->is_used = true;
+				memory_location = current_location;
+				break;
+			}
+		}
+
+		if (memory_location) {
+			/* Memory location has been found */
 			break;
 		}
 	}
 
 	if (!memory_location) {
+		/* All pools have been checked and are exhausted */
 		printf("%s\n", "You're screwed");
 	}
 	memory_location = memory_location + sizeof(mem_control_block);
-	VALGRIND_MEMPOOL_ALLOC(memory_pool_start, memory_location, sizeof(nval));
-	chunks_allocated++;
+	VALGRIND_MEMPOOL_ALLOC(nval_mem_pool[0]->memory_pool_start, memory_location, sizeof(nval));
+	nval_mem_pool[0]->chunks_allocated++;
 	return memory_location;
 }
 
@@ -65,17 +93,17 @@ void nfree(void* p) {
 	mem_control_block* mcb;
 	mcb = p - sizeof(mem_control_block);
 	mcb->is_used = false;
-	VALGRIND_MEMPOOL_FREE(memory_pool_start, p);
-	chunks_allocated--;
+	VALGRIND_MEMPOOL_FREE(nval_mem_pool[0]->memory_pool_start, p);
+	nval_mem_pool[0]->chunks_allocated--;
 	return;
 }
 
 void deallocate_pool(void) {
-	VALGRIND_DESTROY_MEMPOOL(memory_pool_start);
-	free(memory_pool_start);
+	VALGRIND_DESTROY_MEMPOOL(nval_mem_pool[0]->memory_pool_start);
+	free(nval_mem_pool[0]->memory_pool_start);
 	return;
 }
 
 int pool_stats(void) {
-	return chunks_allocated;
+	return nval_mem_pool[0]->chunks_allocated;
 }
--- mempool.h
+++ mempool.h
@@ -1,7 +1,20 @@
 #ifndef nmempool
 #define nmemppol
 
+typedef struct mem_control_block {
+    bool is_used;
+} mem_control_block;
+
+typedef struct memory_pool {
+    int chunks_allocated;
+    void* memory_pool_start;
+    void* memory_pool_end;
+    void* memory_pool_last_assignable;
+    size_t mem_chunk_size;
+} memory_pool;
+
 void allocate_pool(void);
+void create_pool(int pnum);
 void* nmalloc(void);
 void nfree(void* p);
 void deallocate_pool(void);
